4f15dfb49337d4af79e214e2449a597e2a3123dd
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-04 19:28:33 +0100

    first commit

---
+++ Modelo/clase_consulta.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once("clase_conexion.php");
+
+class Consulta
+{
+    public function insertarLibros($id,$titulo,$autor,$paginas)
+    {
+        $conexion= new Conexion();
+        $pdoObject = $conexion->getConexion();
+
+        $sql = "INSERT INTO libros (id, titulo, categoria, precio) VALUES (:id, :titulo, :autor, :paginas)";
+        $sentencia = $pdoObject->prepare($sql);
+
+        $sentencia->bindParam(':id', $id);
+        $sentencia->bindParam(':titulo', $titulo);
+        $sentencia->bindParam(':autor', $autor);
+        $sentencia->bindParam(':paginas', $paginas);
+
+        if (@$sentencia->execute()) {
+            $mensaje = "Registro creado correctamente.";
+        } else {
+            $mensaje = "Fallo al crear el registro.";
+        }
+        return $mensaje;
+    }
+
+    public function cargarLibros()
+    {
+        $modelo = new Conexion();
+        $conexion = $modelo->getConexion();
+
+        $sql = "SELECT * FROM libros";
+        $resultado = $conexion->prepare($sql);
+        $resultado->execute();
+
+        while ($fila = $resultado->fetch())
+        {
+                echo "<tr><td>" . $fila['id'] . "</td><td>" . $fila['titulo'] . "</td><td>" . $fila['autor'] . "</td> <td>" . $fila['paginas'] . "</td> </tr>";
+        }
+        
+        return $rows;
+    }
+}
+?>
---
+++ Modelo/ud04ejer02.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
+    <title>UD 4 Ejercicio 2</title>
+</head>
+<body>
+    
+    <?php
+
+        require_once('clase_consulta.php');
+
+        //Comenzamos insertando los libros:
+        $modelo = new Conexion();
+        $conexion = $modelo->getConexion();
+
+        if(!empty($_POST['id']) && !empty($_POST['autor']) && !empty($_POST['titulo']) && !empty($_POST['paginas'])) 
+        {
+            $id = $_POST['id'];
+            $titulo = $_POST['titulo'];
+            $autor = $_POST['autor'];
+            $paginas = $_POST['paginas'];
+            
+            $insertar = $conexion->insertarLibros($id, $titulo, $autor, $paginas);
+
+            if($resultado){
+                echo "<meta http-equiv='refresh' content='0'>";
+            }
+            
+        }
+    ?>
+
+        <form name="input" action="" method="post">
+            <table id="form">
+                <tr>
+                    <td>
+                        Id:
+                    </td>
+                    <td>
+                        <input type="text" name="id" value = "<?php if (isset($_POST['id'])) echo $_POST['id']; ?>"/>
+                    </td>
+                    <td>
+                        <?php if (isset($_POST['enviar']) && empty($_POST['id'])) echo "<span style='color:red'> Debe introducir el id</span>" ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Título:
+                    </td>
+                    <td>
+                        <input type="text" name="titulo" value = "<?php if (isset($_POST['titulo'])) echo $_POST['titulo']; ?>"/>
+                    </td>
+                    <td>
+                        <?php if (isset($_POST['enviar']) && empty($_POST['titulo'])) echo "<span style='color:red'> Debe introducir un titulo</span>" ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Autor:
+                    </td>
+                    <td>
+                    <input type="text" name="autor" value = "<?php if (isset($_POST['autor'])) echo $_POST['autor']; ?>"/>
+                    </td>
+                    <td>
+                        <?php if (isset($_POST['enviar']) && empty($_POST['autor'])) echo "<span style='color:red'> Debe introducir un autor</span>" ?>                    
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Páginas:
+                    </td>
+                    <td>
+                        <input type="text" name="paginas" value = "<?php if (isset($_POST['paginas'])) echo $_POST['paginas']; ?>"/>    
+                    </td>
+                    <td>
+                        <?php if (isset($_POST['enviar']) && empty($_POST['paginas'])) echo "<span style='color:red'> Debe introducir el número de páginas del libro</span>" ?>                
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Instroducir libros" name="enviar"/>
+                    </td>
+                </tr>
+            </table>
+    </form>
+
+
+    <?php
+
+
+        echo "<div class='tabla'>
+        <table border=2>
+        <tr> 
+        <td> ID </td> 
+        <td> NOMBRE </td> 
+        <td> TITULO </td>
+        <td> PÁGINAS </td>
+        </tr>";
+
+        $insertar = $conexion->cargarLibros();
+
+        echo "</table>";
+
+    ?>
+
+
+</body>
+</html>
