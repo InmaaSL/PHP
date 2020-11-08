93de179795ee24bfdbfbfae6c02d0fb9bbd492b9
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-08 16:15:09 +0100

    Formulario

--- ud05ejer01/ud05ejer01.php
+++ ud05ejer01/ud05ejer01.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,13 +9,20 @@
 </head>
 <body>
     <h1>Registrame</h1>
-    <form action="">
-        <label for="usuario">Usuario</label>
-        <input type="text" id="usuario">
-
-        <label for="constrasena">Contraseña</label>
-        <input type="password" id="contrasena">
-
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td> Usuario:</td>
+                <td><input type="text" name="usuario" value = "<?php if (isset($_POST['usuario'])) echo $_POST['usuario']; ?>"/> </td>
+                <td> <?php if (isset($_POST['enviar']) && empty($_POST['usuario'])) echo "<span style='color:red'> Debe introducir el usuario</span>" ?> </td>
+            </tr>
+            <tr>
+                <td> Constraseña: </td>
+                <td><input type="password" name="contrasena" value = "<?php if (isset($_POST['contrasena'])) echo $_POST['contrasena']; ?>"/></td>
+                <td><?php if (isset($_POST['enviar']) && empty($_POST['contrasena'])) echo "<span style='color:red'> Debe introducir una contraseña</span>" ?></td>
+            </tr>
+        </table>
+        <input type="submit" value="Instroducir libros" name="enviar"/>
     </form>
     
 </body>
