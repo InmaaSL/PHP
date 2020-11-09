4159e052a17169712e12770f4921d748c3f82a06
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-09 16:00:30 +0100

    Tablas

--- ud05ejer02/login.php
+++ ud05ejer02/login.php
@@ -57,12 +57,12 @@ if (isset($_SESSION['usuario'])) {
             <tr>
                 <td> Usuario:</td>
                 <td><input type="text" name="usuario" value = "<?php if (isset($_POST['usuario'])) echo $_POST['usuario']; ?>"/> </td>
-                <td> <?php if (isset($_POST['enviar']) && empty($_POST['usuario'])) echo "<span style='color:red'> Debes introducir el usuario</span>" ?> </td>
+                <td> <?php if (isset($_POST['Entrar']) && empty($_POST['usuario'])) echo "<span style='color:red'> Debes introducir el usuario</span>" ?> </td>
             </tr>
             <tr>
                 <td> Constraseña: </td>
                 <td><input type="password" name="contrasena" value = "<?php if (isset($_POST['contrasena'])) echo $_POST['contrasena']; ?>"/></td>
-                <td><?php if (isset($_POST['enviar']) && empty($_POST['contrasena'])) echo "<span style='color:red'> Debes introducir una contraseña</span>" ?></td>
+                <td><?php if (isset($_POST['Entrar']) && empty($_POST['contrasena'])) echo "<span style='color:red'> Debes introducir una contraseña</span>" ?></td>
             </tr>
         </table>
         <input type="submit" value="Entrar" name="Entrar"/>
