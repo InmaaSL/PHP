9dbceace24c9ca7486bb6fae0f8fa91b757d01e1
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-09 15:42:09 +0100

    Hasta las tablas

--- ud05ejer02/pagina.php
+++ ud05ejer02/pagina.php
@@ -3,18 +3,12 @@
 
     if (!isset ($_SESSION['usuario'])){
     header("Location:login.php");
+    }else{
+        if (isset($_COOKIE['ultimoLogeo'])) {
+            $ultimoLogeo = $_COOKIE['ultimoLogeo'];
+        }
+        setcookie("ultimoLogeo", time(), time()+60*60*24*365);
     }
-    echo "¡Bienvenido, ". $_SESSION['usuario'] ."! En esta página puedes: "; 
-
-    if ($_SESSION['lectura'] == 1)
-        echo ' (ver)';
-    if ($_SESSION['escritura'] == 1)
-        echo ' (escribir)';
-    if ($_SESSION['administracion'] == 1)
-        echo ' (eliminar)';
-
-    echo " libros. <br> Tu última conexión fue el día: ";
-    echo " <br> <a href='logout.php'>Cerrar Sesión</a>";
 ?>
 
 
@@ -26,6 +20,27 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php
+    echo "¡Bienvenido, ". $_SESSION['usuario'] ."! En esta página puedes: "; 
+
+    if ($_SESSION['lectura'] == 1)
+        echo ' (ver)';
+    if ($_SESSION['escritura'] == 1)
+        echo ' (escribir)';
+    if ($_SESSION['administracion'] == 1)
+        echo ' (eliminar)';
+
+
+        if (isset($ultimoLogeo)) {
+            echo " libros. <br> Tu última conexión fue el día: " . @date("d/m/y \a \l\a\s H:i", $ultimoLogeo);
+            }else{
+            echo " libros. <br> ¡Bienvenido por primera vez!";
+            }
+        
+    echo " <br> <a href='logout.php'>Cerrar Sesión</a>";
+?>
+
     <h1>Tabla de libros</h1>
 
     <ul>
