662e55249988bd71fb58e2e61c3b562b6cc717fc
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-21 20:02:44 +0100

    Imagen con marca de agua (imagen): OK!

--- ud06ejer01/05/con_Imagen/logoImagen.php
+++ ud06ejer01/05/con_Imagen/logoImagen.php
@@ -1,28 +1,28 @@
 <?php
     //Primero le cambiamos el tamaño al logo porque es enorme: 
-    // Creamos una imagen cargándola desde el archivo gif
+    //Creamos una imagen cargándola desde el archivo png
     $estampa_org = imagecreatefrompng('logo_TMCM.png');
 
-    // Obtenemos el tercio de las dimensiones de la imagen origen
+    // Obtenemos las dimensiones de la imagen origen:
     $ancho_dst = intval(imagesx($estampa_org)/2);
     $alto_dst = intval(imagesy($estampa_org)/2);
 
-    // Creamos un lienzo para la imagen destino con las dimensiones calculadas
+    // Creamos un lienzo para la imagen destino con las dimensiones calculadas:
     $estampa_dst = imagecreatetruecolor($ancho_dst, $alto_dst);
 
     /* Escalamos la imagen gif origen sobre la imagen nueva destino especificando
     punto de inicio para destino y origen, y punto final para destino y origen. */
     imagecopyresampled($estampa_dst, $estampa_org, 0, 0, 0, 0,
-    $ancho_dst, $alto_dst, imagesx($estampa_org), imagesy($estampa_org));
+                    $ancho_dst, $alto_dst, imagesx($estampa_org), imagesy($estampa_org));
 
-    // Cargar la estampa y la foto para aplicarle la marca de agua
+    // Cargar la estampa y la foto para aplicarle la marca de agua:
     $img = imagecreatefromjpeg('fotoMar.jpg');
 
-    // Establecer los márgenes para la estampa
+    // Establecer los márgenes para la estampa:
     $margen_dcho = 10;
     $margen_inf = 10;
 
-    // obtener el alto/ancho de la imagen de la estampa
+    // Obtener el alto/ancho de la imagen de la estampa:
     $sx = imagesx($estampa_dst);
     $sy = imagesy($estampa_dst);
 
@@ -31,16 +31,14 @@
     imagecopy($img, $estampa_dst, imagesx($img) - $sx - $margen_dcho , imagesy($img) - $sy - $margen_inf, 0, 0, 
             imagesx($estampa_dst), imagesy($estampa_dst));
 
-    // Imprimir y mostrar    // Damos salida a la imagen final cambiando el formato a png
-    // header("Content-type: image/png");
-    // imagepng($estampa_dst);
+    // Imprimir y mostrar, dando salida a la imagen final cambiando el formato a png:
     header('Content-type: image/png');
     imagepng($img);
 
-    // // Guardar en un archivo nuevo
+    //Guardar en un archivo nuevo:
     imagepng($img, "mar_registrado.png");
 
-    // Liberar memoria
+    // Liberaramos memoria
     imagedestroy($img);
     imagedestroy($estampa_org);
     imagedestroy($estampa_dst);
