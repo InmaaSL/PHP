c11382752f70637acb65b6a765f700ae1d7e9968
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-21 19:58:25 +0100

    Crear un lienzo con texto: OK!

--- ud06ejer01/02/cuadrado.php
+++ ud06ejer01/02/cuadrado.php
@@ -1,24 +1,26 @@
 <?php
-// a. Creación y configuración del lienzo
-$alto = 250;
-$ancho = 250;
+    // Creación y configuración del lienzo
+    $alto = 250;
+    $ancho = 250;
+    $imagen = imagecreatetruecolor($ancho, $alto);
 
-$imagen = imagecreatetruecolor($ancho, $alto);
-//Definimos los colores:
-$verdeC = imagecolorallocate($imagen, 63, 191, 127);
-$verdeO = imagecolorallocate($imagen, 12, 38, 25);
+    //Definimos los colores:
+    $verdeC = imagecolorallocate($imagen, 63, 191, 127);
+    $verdeO = imagecolorallocate($imagen, 12, 38, 25);
 
-// b. Dibujamos la imagen
-imagefill($imagen, 0, 0, $verdeO);
-imageline($imagen, 125, $ancho, 125, 0, $verdeC);
-imageline($imagen, 0, 125, $ancho, 125, $verdeC);
+    //Dibujamos la imagen
+    imagefill($imagen, 0, 0, $verdeO);
+    imageline($imagen, 125, $ancho, 125, 0, $verdeC);
+    imageline($imagen, 0, 125, $ancho, 125, $verdeC);
 
-imagestring($imagen, 3, 35, 175, 'BONICO', $verdeC);
-imagestring($imagen, 5, 175, 75, 'FEO', $verdeC);
+    //Escribimos el texto que queremos y lo imprimimos en la imagen: 
+    imagestring($imagen, 3, 35, 175, 'BONICO', $verdeC);
+    imagestring($imagen, 5, 175, 75, 'FEO', $verdeC);
 
-// c. Generación de la imagen
-header('content-type: image/png');
-imagepng ($imagen);
-// d. Liberamos la imagen de memoria
-imagedestroy($imagen);
+    //Generación de la imagen
+    header('content-type: image/png');
+    imagepng ($imagen);
+
+    //Liberamos la imagen de memoria
+    imagedestroy($imagen);
 ?>
