fa453cd7f1197fef45e1f13ff060364dd577273c
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-21 15:42:09 +0100

    FUNCIONA!

--- ud06ejer01/02/cuadrado.php
+++ ud06ejer01/02/cuadrado.php
@@ -11,7 +11,7 @@ $verdeO = imagecolorallocate($imagen, 12, 38, 25);
 // b. Dibujamos la imagen
 imagefill($imagen, 0, 0, $verdeO);
 imageline($imagen, 125, $ancho, 125, 0, $verdeC);
-imageline($imagen, 0, 125, $ancho, 125, $verdeC);
+imageline($imagen, 0, , $ancho, 125, $verdeC);
 
 imagestring($imagen, 3, 35, 175, 'BONICO', $verdeC);
 imagestring($imagen, 5, 175, 75, 'FEO', $verdeC);
