e3c8d54d24dbe10220e5647a5f37fa2dbad9b01b
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-21 20:04:37 +0100

    Foto con marca de agua (texto): OK!

--- ud06ejer01/05/con_texto/logoImagen.php
+++ ud06ejer01/05/con_texto/logoImagen.php
@@ -2,40 +2,37 @@
     //Primero vamos a crear la estampa: 
     $estampa = imagecreatetruecolor(225, 70);
 
-    // dibuja un rectangulo con relleno
+    //Dibuja un rectangulo con relleno:
     imagefilledrectangle($estampa, 0, 0, 200, 50, 0xF4A41A);
     imagefilledrectangle($estampa, 10, 10, 190, 40, 0xEDFAFC);
 
-    //dibuja un texto horizontal
+    //Dibuja un texto horizontal:
     imagestring($estampa, 4, 15, 17, '#TiraMillasConMochila', 0x03171A); 
 
-    // Cargar la estampa y la foto para aplicarle la marca de agua
+    //Cargar la estampa y la foto para aplicarle la marca de agua:
     $img = imagecreatefromjpeg('fotoMar.jpg');
 
-    // Establecer los márgenes para la estampa
+    //Establecer los márgenes para la estampa:
     $margen_dcho = 10;
     $margen_inf = 10;
 
-    // obtener el alto/ancho de la imagen de la estampa
+    //Obtener el alto/ancho de la imagen de la estampa:
     $sx = imagesx($estampa);
     $sy = imagesy($estampa);
 
+    //Copiar la imagen de la estampa sobre nuestra foto usando los índices de
+    // márgen y el ancho de la foto para calcular la posición de la estampa.
     imagecopymerge($img, $estampa, imagesx($img) - $sx - $margen_dcho, imagesy($img) - $sy - $margen_inf,
                 0, 0, imagesx($estampa), imagesy($estampa), 50);
 
-    // Copiar la imagen de la estampa sobre nuestra foto usando los índices de
-    // márgen y el ancho de la foto para calcular la posición de la estampa.
-    // imagecopy($img, $estampa_dst, imagesx($img) - $sx - $margen_dcho , imagesy($img) - $sy - $margen_inf, 0, 0, 
-    //         imagesx($estampa_dst), imagesy($estampa_dst));
-
-    // Imprimir y mostrar 
+    //Imprimir y mostrar:
     header('Content-type: image/png');
     imagepng($img);
 
-    // // Guardar en un archivo nuevo
+    //Guardar en un archivo nuevo
     imagepng($img, "mar_registrado.png");
 
-    // Liberar memoria
+    //Liberar memoria
     imagedestroy($img);
     imagedestroy($estampa);
 ?> 
