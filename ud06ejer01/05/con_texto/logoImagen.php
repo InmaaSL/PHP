3b41df9b229fab64c5fdd44a97ec24f0eb261788
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-21 17:31:02 +0100

    Marca de agua 2 conseguida!!

--- ud06ejer01/05/con_texto/logoImagen.php
+++ ud06ejer01/05/con_texto/logoImagen.php
@@ -1,19 +1,13 @@
 <?php
-    //Primero le cambiamos el tamaño al logo porque es enorme: 
-    // Creamos una imagen cargándola desde el archivo gif
-    $estampa_org = imagecreatefrompng('logo_TMCM.png');
+    //Primero vamos a crear la estampa: 
+    $estampa = imagecreatetruecolor(225, 70);
 
-    // Obtenemos el tercio de las dimensiones de la imagen origen
-    $ancho_dst = intval(imagesx($estampa_org)/2);
-    $alto_dst = intval(imagesy($estampa_org)/2);
+    // dibuja un rectangulo con relleno
+    imagefilledrectangle($estampa, 0, 0, 200, 50, 0xF4A41A);
+    imagefilledrectangle($estampa, 10, 10, 190, 40, 0xEDFAFC);
 
-    // Creamos un lienzo para la imagen destino con las dimensiones calculadas
-    $estampa_dst = imagecreatetruecolor($ancho_dst, $alto_dst);
-
-    /* Escalamos la imagen gif origen sobre la imagen nueva destino especificando
-    punto de inicio para destino y origen, y punto final para destino y origen. */
-    imagecopyresampled($estampa_dst, $estampa_org, 0, 0, 0, 0,
-    $ancho_dst, $alto_dst, imagesx($estampa_org), imagesy($estampa_org));
+    //dibuja un texto horizontal
+    imagestring($estampa, 4, 15, 17, '#TiraMillasConMochila', 0x03171A); 
 
     // Cargar la estampa y la foto para aplicarle la marca de agua
     $img = imagecreatefromjpeg('fotoMar.jpg');
@@ -23,17 +17,18 @@
     $margen_inf = 10;
 
     // obtener el alto/ancho de la imagen de la estampa
-    $sx = imagesx($estampa_dst);
-    $sy = imagesy($estampa_dst);
+    $sx = imagesx($estampa);
+    $sy = imagesy($estampa);
+
+    imagecopymerge($img, $estampa, imagesx($img) - $sx - $margen_dcho, imagesy($img) - $sy - $margen_inf,
+                0, 0, imagesx($estampa), imagesy($estampa), 50);
 
     // Copiar la imagen de la estampa sobre nuestra foto usando los índices de
     // márgen y el ancho de la foto para calcular la posición de la estampa.
-    imagecopy($img, $estampa_dst, imagesx($img) - $sx - $margen_dcho , imagesy($img) - $sy - $margen_inf, 0, 0, 
-            imagesx($estampa_dst), imagesy($estampa_dst));
+    // imagecopy($img, $estampa_dst, imagesx($img) - $sx - $margen_dcho , imagesy($img) - $sy - $margen_inf, 0, 0, 
+    //         imagesx($estampa_dst), imagesy($estampa_dst));
 
-    // Imprimir y mostrar    // Damos salida a la imagen final cambiando el formato a png
-    // header("Content-type: image/png");
-    // imagepng($estampa_dst);
+    // Imprimir y mostrar 
     header('Content-type: image/png');
     imagepng($img);
 
@@ -42,6 +37,5 @@
 
     // Liberar memoria
     imagedestroy($img);
-    imagedestroy($estampa_org);
-    imagedestroy($estampa_dst);
+    imagedestroy($estampa);
 ?> 
