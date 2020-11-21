2d5a5d3b756f7ec23cf4d6be4345f21e33ecbd16
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-21 19:56:58 +0100

    Subir una imagen: OK

--- ud06ejer01/01/imagenPantalla.php
+++ ud06ejer01/01/imagenPantalla.php
@@ -1,12 +1,13 @@
 <?php
-// Creación de la imagen
-$imagen = imagecreatefromjpeg("Galicia.jpg");
-// crea una nueva imagen a partir de un fichero o de una URL.
-// Envío de la imagen
-header("Content-Type: image/jpeg");
-// envia la cabecera del tipo de archivo gráfico que se quiere mostrar.
-imagejpeg($imagen);
-// envia la imagen con imagepng().
-// Liberación de la imagen de memoria
-imagedestroy($imagen);
+    // Creación de la imagen a partir de un fichero o de una URL.
+    $imagen = imagecreatefromjpeg("Galicia.jpg");
+
+    // Envío de la imagen a la cabecera del tipo de archivo gráfico que se quiere mostrar.
+    header("Content-Type: image/jpeg");
+
+    // Envia la imagen con imagepng().
+    imagejpeg($imagen);
+
+    // Liberación de la imagen de memoria
+    imagedestroy($imagen);
 ?> 
