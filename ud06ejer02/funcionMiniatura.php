02aa037219d6ce5301928cd5c4e630f4cc4bdc26
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-22 21:46:36 +0100

    Ejercicio 2: FINIQUITADO DE VERDAD

--- ud06ejer02/funcionMiniatura.php
+++ ud06ejer02/funcionMiniatura.php
@@ -3,8 +3,61 @@
     ini_set('display_errors', 1);     
     ini_set('display_startup_errors',1);     
     error_reporting(E_ALL);
-    
-function miniatura($name_org, $name_dst, $ancho, $alto){
+
+    // Comprobamos si hay un error al subirlo
+    if ($_FILES['imagen']['error'] != UPLOAD_ERR_OK) {
+        echo 'Error: ';
+        switch ($_FILES['imagen']['error'])
+        {
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                echo 'El fichero es demasiado grande';
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                echo 'El fichero no se ha podido subir entero';
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                echo 'No se ha podido subir el fichero';
+                break;
+            default:
+            echo 'Error indeterminado';
+        }
+    // Comprobamos que el fichero es del tipo que esperamos
+    } elseif ($_FILES['imagen']['type'] != 'image/gif' &&
+            $_FILES['imagen']['type'] != 'image/png' &&
+            $_FILES['imagen']['type'] != 'image/jpeg') 
+    {
+        echo 'Error: No se trata de una imagen.';
+    }
+
+    // Si se ha podido subir, lo guardamos.
+    /* Indica si el archivo fue subido mediante HTTP POST. Esto es útil para intentar
+    asegurarse de que un usuario malicioso no ha intentado engañar al script. */
+    elseif (is_uploaded_file($_FILES['imagen']['tmp_name']) === true)
+    {
+        /* Concatenamos la ruta destino y comprobamos si el nombre ya existe previamente,
+        en cuyo caso añadiremos una marca de tiempo. */
+        $nombre = $_FILES['imagen']['name'];
+        if (is_file($nombre) === true)
+        {
+            $nombre = time() . "_" .$nombre;
+        } 
+        
+        // Movemos el fichero a su nueva ubicación
+        // move_uploaded_file ( string $filename , string $destination ) : bool
+        if (move_uploaded_file($_FILES['imagen']['tmp_name'], $nombre))
+        {
+
+            header("Location:ud06ejer02.php?img=" . $_FILES["imagen"]["name"]);
+
+        } else {
+            echo 'Error: No se puede mover el fichero a su destino.';
+        }
+    } else {
+        echo 'Error: posible ataque. Nombre: ' . $_FILES['imagen']['name'];
+    }
+
+    function miniatura($name_org, $name_dst, $ancho, $alto){
         // Separamos el nombre y la extensión en un array de 2 elementos
         $arrNombre = explode(".", $name_org);
         $nombre = $arrNombre[0];
@@ -31,6 +84,6 @@ function miniatura($name_org, $name_dst, $ancho, $alto){
 
     
 $nombre1 = $_FILES['imagen']['name'];
-miniatura($nombre1, "muestra120.jpg", 120, 120);
-miniatura($nombre1, "muestra200.jpg", 200, 200);
+miniatura($nombre1, "120_" . $_FILES['imagen']['name'], 120, 120);
+miniatura($nombre1, "200_" . $_FILES['imagen']['name'], 200, 200);
 ?> 
--- ud06ejer02/ud06ejer02.php
+++ ud06ejer02/ud06ejer02.php
@@ -3,22 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ficheros: </title>
+    <title>UD06EJER02: </title>
 </head>
 <body>
 
     <h1>Elija la foto que desea:</h1>
 
-    <form enctype="multipart/form-data" action="subida.php" method="post">
+    <form enctype="multipart/form-data" action="funcionMiniatura.php" method="post">
         <input type="hidden" name="MAX_FILE_SIZW" value="10000000">
         <label for="imagen">Selecciones el fichero a subir:</label>
         <input type="file" name="imagen" id="imagen">
         <input type="submit" value="Enviar">
     </form>
 
-    <p><img src="muestra120.jpg" alt="Miniatura" /></p>
-    <p><img src="muestra200.jpg" alt="Miniatura" /></p>
+    <?php        
+        ini_set('display_errors', 1); 
+        ini_set('display_startup_errors',1);
+        error_reporting(E_ALL);
 
+        if(isset($_GET['img'])){
+            echo "<p><img src='120_" . $_GET['img']  . "' alt='miniatura'/></p>";
+            echo "<p><img src='200_" . $_GET['img']  . "' alt='miniatura'/></p>";                        
+        }
+    ?>   
 
 </body>
 </html>
