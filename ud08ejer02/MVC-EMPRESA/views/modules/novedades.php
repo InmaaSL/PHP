fc60e60da90e6a73d1d8e35a4c32f7d67cdd6fef
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2021-01-07 12:22:11 +0100

    8.2 OK!

---
+++ ud08ejer02/MVC-EMPRESA/views/modules/novedades.php
@@ -0,0 +1,10 @@
+<h1>PÁGINA DE NOVEDADES</h1>
+<br><br>
+
+Camilo Echeverry Correa (Medellín, 16 de marzo de 1994), también conocido como Camilo, es un músico, 
+cantante, compositor y multinstrumentista colombiano. 
+<br><br>
+Saltó a la fama en 2007 después de ganar el concurso de talentos Factor X de su país.
+Es conocido por sus éxitos «Tutu», junto a Pedro Capó,«Desconocidos» y «La boca», con Mau y Ricky, 
+también es famoso por escribir éxitos para otros cantantes como Becky G y Natti Natasha en «Sin Pijama»
+o Lali y Mau & Ricky en «Sin querer queriendo».
---
+++ ud08ejer02/MVC-EMPRESA/views/modules/ofertas.php
@@ -0,0 +1,15 @@
+<h1>PÁGINA DE OFERTAS</h1>
+
+
+<br><br>
+No es vida de rico,
+Pero se pasa bien rico.
+Y si en la casa no alcanza
+Pa'l aire, te pongo abanico
+<br><br>
+Yo no tengo pa' darte ni un peso,
+Pero sí puedo darte mis besos.
+Pa' sacarte yo tengo poquito,
+Pero es gratis bailar pegaíto'
+<br><br>
+
